451f5cbcb296130f46a2f42788ecb200067cc742
Iyuster <iagogarciaiglesias@gmail.com>
2024-12-12 19:50:44 +0100

    commit 1

---
+++ src/main/java/com/mycompany/nuevoproyecto/NuevoProyecto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nuevoproyecto;
+
+/**
+ *
+ * 
+ * @author 2024-25 DAM1
+ */
+public class NuevoProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } 
+}
